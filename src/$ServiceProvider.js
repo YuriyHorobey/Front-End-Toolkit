cd5540c3d484a25adff2d7c7c7b135585ef2ac28
Yuriy Horobey <yuriy.horobey@gmail.com>
2014-12-24 19:18:33 +0200

    Service Provider

--- src/$ServiceProvider.js
+++ src/$ServiceProvider.js
@@ -1,70 +1,114 @@
 function $ServiceProvider() {
-	var _services = {};
-	return {
-		provides : provides,
-		provide : provide
-	};
+    var _services = {};
+    return {
+        provides: provides,
+        provide: provide
+    };
 
-	function provides(serviceName, serviceCode, isSingleton) {
-		var serviceDescriptor = new ServiceDescriptor(serviceName, serviceCode, isSingleton);
-		_services[serviceName] = serviceDescriptor;
-		return serviceDescriptor;
-	}
+    function provides(serviceName, serviceCode, isSingleton) {
+        var serviceDescriptor = new ServiceDescriptor(this, serviceName, serviceCode, isSingleton);
+        _services[serviceName] = serviceDescriptor;
+        return serviceDescriptor;
+    }
 
-	function provide(serviceName, throwError) {
-		var serviceCode = _services.hasOwnProperty[serviceName] ? _services[serviceName] : null;
-		if (serviceCode === null && throwError) {
-			throw new Error('$ServiceProvider: service "' + serviceName + '" not found');
-		}
-		return serviceCode;
-	}
-	// inner class
-	function ServiceDescriptor(serviceCode, isSingleton) {
-		var _serviceCode;
-		var _isSingleton;
-		var _serviceObject;
-		var _initArguments;
-		return {
-			by : by,
-			asSingleton : asSingleton,
-			initializedWith : initializedWith,
-			get : get
-		};
+    function provide(serviceName, throwError) {
+        var serviceCode = _services.hasOwnProperty(serviceName) ? _services[serviceName] : null;
+        if (serviceCode === null && throwError) {
+            throw new Error('$ServiceProvider: service "' + serviceName + '" not found');
+        }
+        var ret = serviceCode.get();
+        return ret;
+    }
 
-		function by(serviceCode) {
+    // inner class
+    function ServiceDescriptor(serviceProvider, serviceName, serviceCode, isSingleton) {
+        var _serviceProvider;
+        var _serviceName = serviceName;
+        var _serviceCode;
+        var _isSingleton;
+        var _serviceObject;
+        var _initArguments;
+        var _constructor;
+        return {
+            by: by,
+            loadFrom: loadFrom,
+            now: now,
+            asSingleton: asSingleton,
+            initializedWith: initializedWith,
+            provides: provides,
+            get: get
+        };
 
-			return this;
-		}
-		function asSingleton(isSingleton) {
-			if (isSingleton === undefined) {
-				isSingleton = true;
-			}
-			_isSingleton = Boolean(isSingleton);
-			return this;
-		}
-		function initializedWith() {
-			_initArguments = arguments;
-			return this;
-		}
-		function get() {
-			var service;
-			if (_isSingleton) {
-				if (!_serviceObject) {
-					_serviceObject = _createService();
-				}
-				service = _serviceObject;
-			} else {
-				service = _createService();
-			}
-			return service;
+        function by(serviceCode) {
 
-		}
-		function _createService() {
-			function F() {
-				return serviceCode.apply(this, _initArguments);
-			}
-			F.prototype = serviceCode.prototype;
-			return new F();
-		}
-	}
+            return this;
+        }
+
+        function asSingleton(isSingleton) {
+            if (isSingleton === undefined) {
+                isSingleton = true;
+            }
+            _isSingleton = Boolean(isSingleton);
+            return this;
+        }
+
+        function initializedWith() {
+            _initArguments = arguments;
+            return this;
+        }
+
+        function get() {
+            var service;
+            if (_isSingleton) {
+                if (!_serviceObject) {
+                    _serviceObject = _createService();
+                }
+                service = _serviceObject;
+            } else {
+                service = _createService();
+            }
+            return service;
+
+        }
+
+        function loadFrom(url) {
+            return this;
+        }
+
+        function now() {
+            if (!_checkValidity()) {
+                throw new Error('ServiceDescriptor: Invalid data for "' + _serviceName + '"');
+            }
+            return this;
+        }
+
+        function provides(serviceName, serviceCode, isSingleton) {
+            if (!_checkValidity()) {
+                throw new Error('ServiceDescriptor: Invalid data for "' + _serviceName + '"');
+            }
+            return _serviceProvider.provides(serviceName, serviceCode, isSingleton);
+        }
+
+        /**
+         * Checks if this descriptor has enough values to provide a Service.
+         * @private
+         */
+        function _checkValidity() {
+return false;
+        }
+
+        function _locateService() {
+
+        }
+
+        function _createService() {
+            function F() {
+                return _serviceCode.apply(this, _initArguments);
+            }
+
+            F.prototype = serviceCode.prototype;
+            return new F();
+        }
+    }
 }
+
