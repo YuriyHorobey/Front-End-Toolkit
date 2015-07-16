abd6c87c86aaf5edfd96c9e51486953ada1618dc
Yuriy Horobey <yuriy.horobey@gmail.com>
2015-07-16 20:15:21 +0300

    ongoing

--- src/router/router-contract.js
+++ src/router/router-contract.js
@@ -2,10 +2,24 @@
  * Created by yuriy.horobey on 2015-07-14.
  */
 
-function RouterContract(contract) {
-    contract.register('', {
-        pre:     function () {
-        }, post: function () {
+function RouterContract(contract, validator) {
+
+    contract.describe('register', {
+            pre:     function (route, handler) {
+                if (arguments.length < 2) {
+throw new Error();
+                }
+
+            }, post: function () {
+            }
         }
-    });
+    )
+
+    /*
+     contract.register('', {
+     pre:     function () {
+     }, post: function () {
+     }
+     });
+     */
 }
